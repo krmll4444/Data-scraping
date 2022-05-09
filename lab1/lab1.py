1d102d87c2a85cb01aa2ef67b49faddfb7268734
xxwrld <73743934+xxwrld@users.noreply.github.com>
2022-05-09 19:28:37 +0300

    db create

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 from requests import get
 from json import dump
-from re import fullmatch,match
-import json
+from sqlite3 import connect
 
 BASE_URL = 'https://www.khnu.km.ua'
 url = BASE_URL + '/root/page.aspx?l=0&r=3&p=3'
@@ -12,37 +11,41 @@ page = get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 faculties = []
 facult=''
-res={}
+
+i= 0
+
+connections = connect("lab2.db")
+cursor = connections.cursor()
 
 for el in soup.find(id="site-content").find('div').find_all('div'):
     el_h3 = el.find('a').find('h3')
     el_a=el.find('a')
     if el_h3 != None: 
         facult=el_h3.getText()
-        res[el_h3.getText()]=[]
+        for el in faculties:
+            facult.append(el.getText())
+            cursor.execute(
+                "INSERT INTO faculties (name, faculty) VALUES (?, ?)",
+                [el.getText(), i]
+            )
     else:
         if el_a.getText() != "":
-            res[facult].append(el_a.getText())
+            faculties.append(el_a.getText())
+            cursor.execute(
+                "INSERT INTO faculties (name, faculty) VALUES (?, ?)",
+                [el.getText(), i]
+            )
 
-print(res)
+        faculty = {
+            "name": el_h3.getText(),
+        }
+        cursor.execute(
+            "INSERT INTO faculties (name, url) VALUES (?, ?)",
+            [faculty['name']]
+        )
+        faculties.append(faculty)
+        i+=1
+    connections.commit()
 
-with open("lab.json", "w", encoding="utf-8") as json_file:
-    dump(res, json_file, ensure_ascii=False, indent=4)
-# with open("lab1.json", "w", encoding="utf-8") as json_file:
-#     dump(res, json_file, ensure_ascii=False, indent=4)
-#     href_facult = fullmatch(r'/[a-z]+/[a-z]+/[a-z]+', el_a['href'])
-#     if href_facult is not None:
-#         departments = []
-#         try:
-#             chair = el.find(class_="disp_none").find(class_="disp_none").find_all('a')
-#         except Exception:
-#             departments = None
-#         else:
-#             departments = [el.getText() for el in chair]        
+connections.close()
 
-#         faculty = {
-#             "name": el_a.getText(),
-#             "url": href_facult.group(0),
-#             "departments": departments,
-#         }
-#         faculties.append(faculty)
